50b7c466413a8c7272e73919067479d6acf79547
evans-christian2004 <evans.christian2004@gmail.com>
2025-10-12 16:20:12 -0400

    added subtext in hero section

--- src/app/components/LetterCollision.tsx
+++ src/app/components/LetterCollision.tsx
@@ -14,6 +14,16 @@ const lines = [
   'web development UI/UX IT',
 ];
 
+const sub = [
+  "A", 
+  "Software", 
+  "Engineer", 
+  "and", 
+  "Information", 
+  "Technology", 
+  "Professional",
+]
+
 function getRandomRotation(){
     // 
     return Math.random() * 60 - 30;
@@ -53,18 +63,26 @@ export function LetterCollision() {
       };
     }, []);
     return (
-      <div ref={ref} className="ml-24 scroll-smooth">
+      <div ref={ref} className="ml-8 xs:ml-8 sm:ml-12 md:ml-18 xl:ml-24  scroll-smooth select-none">
         {/* layout for two lines + sub-sentence */}
         <div className="-mt-24 mb-36 flex h-screen flex-col justify-end lg:mb-24">
-          <div className="flex letter text-6xl font-semibold xs:text-[90px] xs:leading-none md:text-[120px] lg:text-[150px] xl:text-[210px] ">
+          <div className="letter flex text-7xl font-semibold xs:text-[90px] xs:leading-none md:text-[120px] lg:text-[150px] xl:text-[210px] ">
             <LetterDisplay word={lines[0]} />
-            <div className="w-4 sm:w-10" />
+            <div className="w-4 sm:w-5" />
             <LetterDisplay word={lines[1]} />
           </div>
-          <div className="flex flex-wrap text-6xl font-semibold xs:text-[90px] xs:leading-none md:text-[120px] lg:text-[150px] xl:text-[210px] text-primay-500 text-primary-500">
+          <div className="flex flex-wrap text-7xl font-semibold xs:text-[90px] xs:leading-none md:text-[120px] lg:text-[150px] xl:text-[210px] text-primay-500 text-primary-500">
             <LetterDisplay word={lines[2]} />
             <div className="w-4 sm:w-10" />
-            
+          </div>
+          <div className="flex flex-wrap text-3xl xs:text-[16px] xs:leading-none mt-2 ml-4 md:text-[30px] lg:text-[35px] xl:text-[50px] text-primay-500">
+            {sub.map((word, index) => (
+              <div key={index} className="flex items-center">
+                <LetterDisplay word={word} />
+                {/* whitespace divider, except after last item */}
+                {index < sub.length - 1 && <div className="w-2 sm:w-2" />}
+              </div>
+            ))}
           </div>
         </div>
         <div className="flex flex-wrap text-6xl font-semibold xs:text-[90px] xs:leading-none md:text-[120px] lg:text-[150px] xl:text-[210px]">
