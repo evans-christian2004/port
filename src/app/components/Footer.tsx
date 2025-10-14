f7d8f1439c5a6db19be0afc40d7cf7c8611176ed
evans-christian2004 <67699103+evans-christian2004@users.noreply.github.com>
2025-10-13 22:06:18 -0400

    implimented footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,16 +4,26 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="bg-primary-500 text-background h-40">
-        <div className='flex justify-around items-center pt-6 pb-12'>
-            <div className="flex gap-1">
-                <Link className='bg-primary-400 rounded-full p-2' href="https://github.com/evans-christian2004"><Image className='svg-hover' src="/github-dark.svg" width={35} height={35} alt='github'/></Link>
-                <Link className='bg-primary-400 rounded-full p-2' href="https://www.linkedin.com/in/evanschristian/"><Image src="/linkedin-dark.svg" width={35} height={35} alt='github'/></Link>
-                <Link className='bg-primary-400 rounded-full p-2' href="/CSResume.pdf"><Image src="/resume-dark.svg" width={35} height={35} alt='github'/></Link>
+    <div className="bg-primary-500 text-background md:h-36 h-60">
+        <div className='flex md:flex-row flex-col gap-5 justify-around items-center pt-6 pb-6'>
+            <div className="px-12">
+              <Image 
+                className='logo-shake transition-all ease-in-out'
+                src="/Logo-dark.svg"
+                width={50}
+                height={50}
+                alt='logo'
+              />
             </div>
             <div className="">
                 Made with <Image className='inline' src="/heart.svg" width={32} height={32} alt="love"/> by Chris
             </div>
+            <div className="flex gap-1">
+                <Link className='bg-primary-400 rounded-full p-2' href="https://github.com/evans-christian2004"><Image className='' src="/github-dark.svg" width={35} height={35} alt='github'/></Link>
+                <Link className='bg-primary-400 rounded-full p-2' href="https://www.linkedin.com/in/evanschristian/"><Image src="/linkedin-dark.svg" width={35} height={35} alt='github'/></Link>
+                <Link className='bg-primary-400 rounded-full p-2' href="/CSResume.pdf"><Image src="/resume-dark.svg" width={35} height={35} alt='github'/></Link>
+            </div>
+            
         </div>
         <p className='text-xs text-background w-fit mx-auto'>&#169; Christian Evans. all rights reserved</p>
     </div>
