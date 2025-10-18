00bd6e15c5d6e172e1619485e6ef1ff979d48a98
evans-christian2004 <evans.christian2004@gmail.com>
2025-10-17 21:11:25 -0400

    added skills section icons. need to add icons for certs

--- src/app/components/SkillsSection.tsx
+++ src/app/components/SkillsSection.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { skills } from '../data/skills'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const iconSize : number = 120;
 
 const SkillsSection = () => {
   return (
-    <div className='container mx-auto p-10'>
+    <div className='container mx-auto p-10 min-h-screen'>
         <h1 className='text-6xl'>Skills:</h1>
-        
+        {skills.map((category) => (
+            <div key={category.category}>
+                <h2 className='mt-10 mb-8'>{category.category}</h2>
+                <ul className="flex flex-wrap gap-5">
+                    {category.skills.map((skill) => (
+                        <li key={skill.name} className='w-9 h-9 p-2 bg-secondary-500'>
+                            <Link href={skill.url}>
+                                <Image src={skill.iconSrc} height={iconSize} width={iconSize} alt={`${skill.name} icon`}/>
+                                <h3>{skill.name}</h3>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        ))}
     </div>
   )
 }
--- src/app/data/skills.ts
+++ src/app/data/skills.ts
@@ -1,9 +1,9 @@
 import React from "react";
 
 export type Skill = {
-    name?: string;
-    iconSrc?: string;
-    url?: string;
+    name: string;
+    iconSrc: string;
+    url: string;
 };
 
 export type SkillCategory = {
@@ -13,147 +13,132 @@ export type SkillCategory = {
 
 export const skills: SkillCategory[] = [
     {
-        category: "languages",
+        category: "Languages",
         skills: [
             {
                 name: "JavaScript",
-                iconSrc: "",
+                iconSrc: "/icons/skill-icons/javascript.svg",
                 url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
             },
             {
                 name: "TypeScript",
-                iconSrc: "",
-                url: "https://www.typescriptlang.org/docs/",
-            },
-            {
-                name: "Python",
-                iconSrc: "",
-                url: "https://docs.python.org/3/",
-            },
-            {
-                name: "C",
-                iconSrc: "",
-                url: "https://devdocs.io/c/",
-            },
-        ],
-    },
-    {
-        category: "frameworks",
-        skills: [
-            {
-                name: "React",
-                iconSrc: "",
-                url: "https://react.dev/",
-            },
-            {
-                name: "Next.js",
-                iconSrc: "",
-                url: "https://nextjs.org/docs",
-            },
-            {
-                name: "Node.js",
-                iconSrc: "",
-                url: "https://nodejs.org/en/docs",
-            },
-            {
-                name: "Tailwind CSS",
-                iconSrc: "",
-                url: "https://tailwindcss.com/docs",
-            },
-            {
-                name: "MongoDB",
-                iconSrc: "",
-                url: "https://www.mongodb.com/docs/",
-            },
-            {
-                name: "Mongoose",
-                iconSrc: "",
-                url: "https://mongoosejs.com/docs/",
+                iconSrc: "/icons/skill-icons/typescript.svg",
+                url: "https://www.typescriptlang.org/",
             },
             {
                 name: "HTML",
-                iconSrc: "",
+                iconSrc: "/icons/skill-icons/html.svg",
                 url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
             },
             {
                 name: "CSS",
-                iconSrc: "",
+                iconSrc: "/icons/skill-icons/css.svg",
                 url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+            },
+            {
+                name: "Python",
+                iconSrc: "/icons/skill-icons/python.svg",
+                url: "https://www.python.org/",
+            },
+            {
+                name: "C",
+                iconSrc: "/icons/skill-icons/c.svg",
+                url: "https://devdcs.io/c/",
             },
         ],
     },
     {
-        category: "tools",
+        category: "Frameworks",
+        skills: [
+            {
+                name: "React",
+                iconSrc: "/icons/skill-icons/react.svg",
+                url: "https://react.dev/",
+            },
+            {
+                name: "Next.js",
+                iconSrc: "/icons/skill-icons/nextjs.svg",
+                url: "https://nextjs.org/",
+            },
+            {
+                name: "Node.js",
+                iconSrc: "/icons/skill-icons/nodejs.svg",
+                url: "https://nodejs.org/en/",
+            },
+            {
+                name: "Tailwind CSS",
+                iconSrc: "/icons/skill-icons/tailwind.svg",
+                url: "https://tailwindcss.com/",
+            },
+            {
+                name: "tRPC",
+                iconSrc: "/icons/skill-icons/trpc.svg",
+                url: "https://trpc.io/",
+            },
+            {
+                name: "Prisma",
+                iconSrc: "/icons/skill-icons/prisma.svg",
+                url: "https://www.prisma.io/"
+            }
+        ],
+    },
+    {
+        category: "Tools",
         skills: [
             {
                 name: "Git",
-                iconSrc: "",
+                iconSrc: "/icons/skill-icons/git.svg",
                 url: "https://git-scm.com/doc",
             },
             {
                 name: "GitHub",
-                iconSrc: "",
+                iconSrc: "/icons/skill-icons/github.svg",
                 url: "https://docs.github.com/en",
             },
             {
+                name: "Docker",
+                iconSrc: "/icons/skill-icons/docker.svg",
+                url: "https://www.docker.com/"
+            },
+            {
                 name: "Figma",
-                iconSrc: "",
+                iconSrc: "/icons/skill-icons/figma.svg",
                 url: "https://help.figma.com/hc/en-us",
             },
             {
                 name: "Adobe Illustrator",
-                iconSrc: "",
+                iconSrc: "/icons/skill-icons/illustrator.svg",
                 url: "https://helpx.adobe.com/illustrator/user-guide.html",
             },
             {
                 name: "Microsoft Office",
-                iconSrc: "",
+                iconSrc: "/icons/skill-icons/office.svg",
                 url: "https://support.microsoft.com/en-us/office",
             },
             {
                 name: "Unity",
-                iconSrc: "",
+                iconSrc: "/icons/skill-icons/unity.svg",
                 url: "https://docs.unity3d.com/Manual/index.html",
-            },
-            {
-                name: "Active Directory",
-                iconSrc: "",
-                url: "https://learn.microsoft.com/en-us/windows-server/identity/active-directory-domain-services",
             },
         ],
     },
     {
-        category: "apis and integrations",
+        category: "APIs and Integrations",
         skills: [
             {
                 name: "Google Gemini API",
-                iconSrc: "",
+                iconSrc: "/icons/skill-icons/gemini.svg",
                 url: "https://ai.google.dev/docs",
-            },
-            {
-                name: "ElevenLabs TTS",
-                iconSrc: "",
-                url: "https://elevenlabs.io/docs",
-            },
-            {
-                name: "pdfjs-dist",
-                iconSrc: "",
-                url: "https://mozilla.github.io/pdf.js/",
-            },
+            }
         ],
     },
     {
-        category: "certifications",
+        category: "Certifications",
         skills: [
             {
                 name: "CodePath TIP 102",
                 iconSrc: "",
                 url: "https://www.codepath.org/courses/tech-interview-prep",
-            },
-            {
-                name: "CodePath WEB 102",
-                iconSrc: "",
-                url: "https://www.codepath.org/courses/web-development",
             },
             {
                 name: "CompTIA IT Fundamentals Pro",
