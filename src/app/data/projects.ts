7e2b3c4fb054285c193b654505f6733645239c59
evans-christian2004 <evans.christian2004@gmail.com>
2025-10-27 22:02:53 -0400

    added beginner projects.

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -16,8 +16,8 @@ type project = {
     repository: string;
     devPost?: string;
     demo?: string;
-    isWinner: boolean;
-    PrimaryColor?: string;
+    isWinner?: boolean; //this is personal lmao, I gotta win one of these hackathons bro
+    primaryColor?: string;
     secondaryColor?: string;
 }
 
@@ -32,11 +32,11 @@ const projects: project[] = [
         ],
         bodyText: "",
         images: [
-
+            
         ],
         repository: "https://github.com/evans-christian2004/alto-starter",
         isWinner: false,
-        PrimaryColor: "",
+        primaryColor: "",
         secondaryColor: "",
     },
     {
@@ -45,15 +45,15 @@ const projects: project[] = [
         description: 
             "The ultimate learning assistant! TL;DR can take in multiple forms of input and format them into different mediums using the Gemini API for people who what their media in a different form",
         tags: [
-             "Google Gemini API", "EllevenLabs API","TypeScript", "Next.js", "React", "Tailwind",
+             "Google Gemini API", "EllevenLabs API","TypeScript", "Next.js", "React", "Tailwind"
         ],
         bodyText: "",
         images: [
 
         ],
-        repository: "",
+        repository: "https://github.com/evans-christian2004/the-tldr-app",
         isWinner: false,
-        PrimaryColor: "",
+        primaryColor: "",
         secondaryColor: "",
     },
     {
@@ -61,7 +61,7 @@ const projects: project[] = [
         subtext: "Your Next Foundation Exam Prep Tool",
         description: "The University of Central Florida as of 20205 uses a foundation exam to determine which students remain in the Computer Science major and which get weeded out. This tool allows for users to practice for exam by whiteboarding answers to problems with auto grading using the Google Gemini API.",
         tags: [
-            "TLDraw API", "TavaScript", "React", "Tailwind",
+            "TLDraw API", "TavaScript", "React", "Tailwind"
         ],
         bodyText: "",
         images: [
@@ -69,21 +69,22 @@ const projects: project[] = [
         ],
         repository: "https://github.com/sebastian-noel/FE-AI",
         isWinner: false,
+        primaryColor: "",
+        secondaryColor: "",
     },
     {
         name: "Sanity Blog Implimentation",
         subtext: "An Experiment With the Sanity CMS",
         description: "Sanity is a headless CMS platform that allows developers and content creators to more easily manage content. I wanted to try out this new product as a developer tool, So I made a simple project with it and learned a lot about CMS systems!",
         tags: [
-            "Sanity", "TypeScript", "Next.js", "React", "Tailwind",
+            "Sanity", "TypeScript", "Next.js", "React", "Tailwind"
         ],
         bodyText: "",
         images: [
 
         ],
         repository: "https://github.com/evans-christian2004/sanity-blog",
-        isWinner: false,
-        PrimaryColor: "",
+        primaryColor: "",
         secondaryColor: "",
     },
     {
@@ -98,8 +99,7 @@ const projects: project[] = [
 
         ],
         repository: "https://github.com/evans-christian2004/creatorverse",
-        isWinner: false,
-        PrimaryColor: "",
+        primaryColor: "",
         secondaryColor: "",
     },
     {
@@ -107,32 +107,45 @@ const projects: project[] = [
         subtext: "A TypeScript Learning Flash Card Game",
         description: "Before learning TypeScript, I wanted to become more familiar with the concepts, so I made a TypeScript flashcard game with react before jumping in to using the JavaScript Alternative",
         tags: [
-            "React", "JavaScript", "HTML", "CSS",
+            "React", "JavaScript", "HTML", "CSS"
         ],
         bodyText: "",
         images: [
 
         ],
         repository: "https://github.com/evans-christian2004/flashcards",
-        isWinner: false,
-        PrimaryColor: "",
+        primaryColor: "",
         secondaryColor: "",
     },
     {
         name: "305 Life",
         subtext: "See What Miami Has to Offer",
-        description: "",
+        description: "Miami has a lot to offer, and I wanted to show off my bustling home. As my first React project, I use the framework to show off some of the amazing sites and activities that can be done in Miami, Florida",
         tags: [
-            "React", "JavaScript", "HTML", "CSS",
+            "React", "JavaScript", "HTML", "CSS"
         ],
         repository: "https://github.com/evans-christian2004/community-board",
-        bodyText: "Miami has a lot to offer, and I wanted to show off my bustling home. As my first React project, I use the framework to show off some of the amazing sites and activities that can be done in Miami, Florida",
+        bodyText: "",
         images: [
 
         ],
-        isWinner: false,
-        PrimaryColor: "",
+        primaryColor: "",
         secondaryColor: "",
+    },
+    {
+        name: "BlackJack",
+        subtext: "Blackjack Imoplimentation with just HTML, CSS, and JS",
+        description: "In order to get a feel for web development technologies, I began to the basics. This is a simple Blackjack implientation in vanilla HTML, CSS, and JavaScript. You can hit, stand, and there's a dealer.",
+        tags: [
+            "JavaScript", "HTML", "CSS"
+        ],
+        repository: "https://github.com/evans-christian2004/Black-Jack",
+        bodyText: "",
+        images: [
+
+        ],
+        primaryColor: "",
+        secondaryColor: ""
     },
 ]
 
